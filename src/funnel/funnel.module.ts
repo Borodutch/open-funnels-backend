30547f1811f0ca590c8a47f8a051f07242fe4da4
Stas N <stas-nepomn@ya.ru>
2021-04-07 23:38:19 +0800

    Funnel model module injection

--- src/funnel/funnel.module.ts
+++ src/funnel/funnel.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FunnelController } from './funnel.controller';
 import { FunnelService } from './funnel.service';
+import { Funnel, FunnelSchema } from './models/funnel.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Funnel.name, schema: FunnelSchema }]),
+  ],
   controllers: [FunnelController],
-  providers: [FunnelService]
+  providers: [FunnelService],
 })
 export class FunnelModule {}
