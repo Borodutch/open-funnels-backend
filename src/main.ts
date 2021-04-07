d53ae5476dfdac4ac603d787590fc73a67850ba1
Stas N <stas-nepomn@ya.ru>
2021-04-07 22:55:51 +0800

    Server file updated

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: true });
   const configService = app.get(ConfigService);
   const port = configService.get('port');
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          styleSrc: [
+            `'self'`,
+            `'unsafe-inline'`,
+            'cdn.jsdelivr.net',
+            'fonts.googleapis.com',
+          ],
+          fontSrc: [`'self'`, 'fonts.gstatic.com'],
+          imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`, `cdn.jsdelivr.net`],
+        },
+      },
+    }),
+  );
   app.setGlobalPrefix('v1');
   await app.listen(port);
   console.log(`Open funnels backend is running on ${await app.getUrl()}`);
