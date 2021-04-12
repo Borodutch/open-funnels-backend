d97524651a5026aacede19b99bee3d04348f60ab
Stas N <stas-nepomn@ya.ru>
2021-04-12 21:43:32 +0800

    Documentation update

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -39,6 +39,11 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <span class="icon ion-ios-paper"></span>LICENSE
                             </a>
                         </li>
+                        <li class="link">
+                            <a href="todo.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>TODO
+                            </a>
+                        </li>
                                 <li class="link">
                                     <a href="dependencies.html" data-type="chapter-link">
                                         <span class="icon ion-ios-list"></span>Dependencies
