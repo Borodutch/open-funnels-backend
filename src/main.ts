766f677bf108d385a525730b736b7cc4352c36a4
Stas N <stas-nepomn@ya.ru>
2021-04-16 00:19:56 +0800

    Pretty colors in terminal

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: false,
-    cors: true,
-  });
-  const configService = app.get(ConfigService);
-  const port = configService.get('port');
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-    }),
-  );
-  app.setGlobalPrefix('api');
-  await app.listen(port);
-  console.log(`Open funnels backend is running on ${await app.getUrl()}`);
+  try {
+    const app = await NestFactory.create(AppModule, {
+      logger: false,
+      cors: true,
+    });
+    const configService = app.get(ConfigService);
+    const port = configService.get('port');
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+      }),
+    );
+    app.setGlobalPrefix('api');
+    await app.listen(port);
+    console.log(
+      '\x1b[1m\x1b[42m\x1b[37m Open funnels',
+      '\x1b[0m\x1b[36m started:',
+      '\x1b[0mbackend is running on',
+      `\x1b[1m${await app.getUrl()}`,
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 bootstrap();
